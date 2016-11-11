b095abc06ed725f0629a33299b2918d1b04d6afc
Federico Bond <federicobond@gmail.com>
2016-11-10 23:51:55 -0300

    Finish script and add README.md

---
+++ README.md
@@ -0,0 +1,36 @@
+Solidity Spec Suite
+===================
+
+This suite has been extracted mostly from the official Solidity compiler tests
+and contains a list of `.sol` files that should either pass or fail in a
+compliant solidity parser. It does not attempt to be complete or extensive at
+this point, just a starting point for Solidity parser authors.
+
+There is a test runner at `./run-tests.sh`. To test your parser against it, run:
+
+```
+./run-tests.sh -p ../path/to/your/parser/executable
+```
+
+Your parser should exit with status 0 if it parsed the input successfully or >0
+otherwhise. The output from each test is collected in `output/` so you can
+analyze it later.
+
+The tool will run all the code examples and report accordingly.
+
+If you are working on getting a specific test to pass, you can run the script
+with the `-f <glob>` option to filter the tests by a certain pattern.
+
+For example:
+
+```
+./run-tests.sh -p myparser -f "*enum*"
+```
+
+## License
+
+Code is licensed under the GPL.
+
+## Author
+
+The Solidity Spec Suite is developed by [Federico Bond](https://github.com/federicobond)
--- run-tests.sh
+++ run-tests.sh
@@ -1,59 +1,97 @@
-#!/bin/sh
+#!/bin/bash
 
 RED="$(tput bold)$(tput setab 1)"
 GREEN="$(tput bold)$(tput setab 2)$(tput setaf 0)"
 RESET="$(tput sgr0)"
 
-PARSER="../solidity-parser/cli.js"
-#PARSER="../solidity2/build/solc/solc"
+PARSER="solc"
 
-passes=0
-fails=0
+PASSES=0
+FAILS=0
+
+die () {
+  >&2 echo "error: $1"
+  exit 1
+}
 
 pass () {
   echo "$GREEN PASS $RESET $1"
-  passes=$((passes+1))
+  PASSES=$((PASSES + 1))
 }
 
 fail () {
   echo "$RED FAIL $RESET $1"
-  fails=$((fails+1))
+  FAILS=$((FAILS + 1))
 }
 
 assert_pass () {
-  $PARSER $1 > results/$1 2>&1
+  $PARSER "$1" > "output/$1" 2>&1
   if [ $? -eq 0 ]; then
-    pass $1
+    pass "$1"
   else
-    fail $1
+    fail "$1"
   fi
 }
 
 assert_fail () {
-  $PARSER $1 > results/$1 2>&1
+  $PARSER "$1" > "output/$1" 2>&1
   if [ $? -ne 0 ]; then
-    pass $1
+    pass "$1"
   else
-    fail $1
+    fail "$1"
   fi
 }
 
-main () {
-  mkdir -p results/{passes,fails}
+filter_file () {
+  if [ -z "$FILTER" ]; then
+    return 0
+  fi
 
+  [[ "$1" == $FILTER ]]
+}
+
+test_init () {
+  if [ ! -f "$PARSER" ]; then
+    die "could not find parser: $PARSER"
+  fi
+
+  mkdir -p output/{passes,fails}
+}
+
+test_execute () {
   for test in passes/*.sol; do
-    assert_pass "$test"
+    if filter_file "$test"; then
+      assert_pass "$test"
+    fi
   done
 
   for test in fails/*.sol; do
-    assert_fail "$test"
+    if filter_file "$test"; then
+      assert_fail "$test"
+    fi 
   done
-
-  printf "\n%s tests passed, %s tests failed.\n" $passes $fails
-  
-  if [ $fails -gt 0 ]; then
-    exit 1
-  fi
 }
 
-main
+test_finalize () {
+  printf "\n%s tests passed, %s tests failed.\n" "$PASSES" "$FAILS"
+}
+
+usage () {
+  echo "usage: ./run_tests.sh [-p <parser>] [-f <glob>]"
+  exit
+}
+
+while getopts f:p:h opt; do
+  case $opt in
+    p) PARSER=$OPTARG;;
+    f) FILTER=$OPTARG;;
+    h) usage;;
+   \?) exit 64;;
+  esac
+done
+
+test_init
+test_execute
+test_finalize
+
+[ $FAILS -gt 0 ] && exit 1 || exit 0
